698f65aef5c50e95835c79dae27c608f943bf053
Evan Kasky <ekasky25@gmail.com>
2026-07-15 00:31:50 -0400

    add a domain specific excpetion class to derive all other exceptions for the simulation

---
+++ backend/src/main/java/com/evankasky/backend/exception/PowerGridSimulationException.java
@@ -0,0 +1,9 @@
+package com.evankasky.backend.exception;
+
+public class PowerGridSimulationException extends RuntimeException {
+
+    protected PowerGridSimulationException(String message) {
+        super(message);
+    }
+
+}
